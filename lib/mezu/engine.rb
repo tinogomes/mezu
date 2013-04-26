44db3e8f7a928a6e780dcae42b4543ea930d7cc9
tinogomes <tinorj@gmail.com>
2013-04-26 19:43:47 -0300

    Ignoring load generators if not have Rails::Engine

--- lib/mezu/engine.rb
+++ lib/mezu/engine.rb
@@ -1,14 +1,16 @@
-module Mezu
-  class Engine < Rails::Engine
-    generators do
-      require "mezu/generators/install"
-      require "mezu/generators/copy_locales"
-    end
+if defined?(Rails)
+  module Mezu
+    class Engine < Rails::Engine
+      generators do
+        require "mezu/generators/install"
+        require "mezu/generators/copy_locales"
+      end
 
-    config.after_initialize do
-      if Config.on_load
-        Config.on_load[Config]
+      config.after_initialize do
+        if Config.on_load
+          Config.on_load[Config]
+        end
       end
     end
-  end
+  end 
 end
